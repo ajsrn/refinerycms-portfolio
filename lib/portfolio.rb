52d17c9c156161195467be4267cf52712b3639bb
Alastair Surin <asurin@weddingwire.com>
2011-12-14 13:55:08 -0500

    fix

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -1,5 +1,5 @@
 require 'portfolio/version'
 
 unless defined?(::Refinery::Portfolio::Engine)
-  require File.expand_path('../refinerycms-portfolio.rb', __FILE__)
+  require File.expand_path('./refinerycms-portfolio.rb', __FILE__)
 end
